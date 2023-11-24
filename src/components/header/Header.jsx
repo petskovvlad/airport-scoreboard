a75fa63b4843e45c96e350cd097aef38b161d390
Vladyslav Petskkov <petskovvlad@gmail.com>
2023-11-24 03:28:04 +0200

    add svg header logo

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,13 +1,13 @@
 import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlane } from "@fortawesome/free-solid-svg-icons";
+
 import './header.scss';
 
 const Header = () => {
   return (
     <div className="header">
-        <img 
-          className="header__logo" 
-          src="https://iev.aero/_nuxt/img/logo@2x.2d2c20b.png" 
-          alt="logo" />
+          <FontAwesomeIcon icon={faPlane} className="header__logo" />
     </div>
   )
 }
